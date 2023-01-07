f0a44c13f6f99b423c4a01bd5b889e103be8a85e
Superdev0918 <developerdev52212@gmail.com>
2023-01-07 19:01:15 +0900

    git init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="mainBkg">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
